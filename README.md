219edd537f3fff9d03784322e84b8b2c6e441025
Mal Curtis <mal@mal.co.nz>
2016-09-03 14:41:34 +1200

    Initial playing around

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# escea
-Python Client for Escea Fireplaces
+# Escea Fireplace Python Client
+
+Use this library to access and control Escea Fires over the local network using the proprietary Escea UDP message protocol. This is a work in progress and should be considered hobbyist / alpha software.
+
+# Features
+
+- [ ] Discover broadcasting fires
+- [ ] Authenticate with pin code*
+- [x] Retrieve Fire State*
+- [ ] Turn fire on*
+- [ ] Increase / Decrease Target temperature*
+- [ ] Turn on flame effect*
+- [ ] Turn on fan boost*
+- [ ] Turn on shedules
+- [ ] Parity Checking
+
+* Planned features
+
+# Example
+
+```py
+fire = Fire(FIRE_IP, '47', '46')
+fire.start(LOCAL_IP)
+
+resp = fire.send(fire.stateRequest())
+print >>sys.stderr, resp.state
+# {'current_temp': 19, 'on': False, 'target_temp': 23, 'fan_boost': True, 'flame_effect': True}
+  ```
+
+# API
+
+I have only discovered the control api information by dumping udp traffic from the official Escea iPhone application while talking to an [Escea DX1500](http://www.escea.com/nz/fireplaces/indoor-fireplaces/dx-series/dx1500/).
+
+The api is a UDP "send / receive" api, where payloads are 15 bytes consisting of 15 hexadecimal commands codes. Commands are sent and received on port 3300.
+
+## API Protocol
+
+Each portion of the message contains a code that has a certain meaning.
+
+* 1: Unknown, perhaps a code specific to the pin? Perhaps received in the initial authentication. Mine is `47`.
+* 2: Command code. This appears to describe what the entire messages function is.
+* 3-13: Data codes. For a command message, this is all `00`, e.g. `00:00:00:00:00:00:00:00:00:00:00`. For a state response, each portion maps to a specific piece of information, e.g. `06:00:00:00:00:07:13:00:00:00:00`, where `07` is target temp and `13` is current temp (note this is hex for the decimal `19`).
+* 14: This appears to be a parity command / digest command that confirms the entire message is correct. This can be best seen with either an increment response such as an info command when the temp increases by 1, as the parity bit also increases by 1, or in a command message where the parity bit matches the command (since the rest of the message is `00`).
+* 15: Unknown, looks to somehow be related to the first command code? Mine is `46`.
+>>>>>>> Initial playing around
---
+++ test.py
@@ -0,0 +1,121 @@
+import socket
+import sys
+
+# Create a UDP socket
+LOCAL_IP = "192.168.0.107"
+FIRE_IP = '192.168.0.130'
+UDP_PORT = 3300
+
+message = '473100000000000000000000003146'.decode('hex')
+
+class Fire(object):
+    def __init__(self, ip, prefix, suffix):
+        super(Fire, self).__init__()
+        self.ip = ip
+        self.prefix = prefix
+        self.suffix = suffix
+
+    def start(self, ip):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind((ip, UDP_PORT))
+
+    def stop(self):
+        self.sock.close()
+
+    def stateRequest(self):
+        return StateRequest(self.prefix, self.suffix)
+
+    def send(self, message):
+        self.sock.sendto(message.hex(), (self.ip, UDP_PORT))
+        data, server = self.sock.recvfrom(1024)
+        return ResponseMessage(data.encode('hex')).Response()
+
+
+class Message(object):
+
+    def __init__(self):
+        super(Message, self).__init__()
+        self.parts = [ '00' for y in range( 15 ) ]
+
+    def set(self, index, value):
+        self.parts[index] = value
+
+    def get(self, index):
+        return self.parts[index]
+
+    def string(self):
+        str = ''
+        for y in self.parts:
+            str += y
+        return str
+
+    def hex(self):
+        return self.string().decode('hex')
+
+class RequestMessage(Message):
+    def __init__(self, prefix, suffix):
+        super(RequestMessage, self).__init__()
+        self.set(0, prefix)
+        self.set(14, suffix)
+
+    def command(self, value):
+        self.parts[1] = value
+        self.parts[13] = value
+
+class StateRequest(RequestMessage):
+    def __init__(self, prefix, suffix):
+        super(StateRequest, self).__init__(prefix, suffix)
+        self.command('31')
+
+class UnknownResponse(object):
+    def __init__(self, message):
+        super(UnknownResponse, self).__init__()
+        self.message = message
+
+class StateResponse(object):
+    BOOL={'00': False, '01': True}
+
+    def __init__(self, message):
+        super(StateResponse, self).__init__()
+        self.state = {
+            "target_temp": self.int(message.get(7)),
+            "current_temp": self.int(message.get(8)),
+            "on": self.bool(message.get(4)),
+            "fan_boost": self.bool(message.get(5)),
+            "flame_effect": self.bool(message.get(6)),
+        }
+
+    def int(self, value):
+        return int(value, base=16)
+
+    def bool(self, value):
+        return StateResponse.BOOL[value]
+
+class ResponseMessage(Message):
+    MESSAGES={'80': StateResponse}
+    def __init__(self, message):
+        super(ResponseMessage, self).__init__()
+        parts = map(''.join, zip(*[iter(message)]*2))
+        i = 0
+        for part in parts:
+            self.set(i, part)
+            i += 1
+
+    def Response(self):
+        try:
+            klass = self.MESSAGES[self.get(1)]
+        except KeyError, e:
+            return UnknownResponse(self)
+        return klass(self)
+
+
+try:
+
+    fire = Fire(FIRE_IP, '47', '46')
+    fire.start(LOCAL_IP)
+
+    resp = fire.send(fire.stateRequest())
+    print >>sys.stderr, resp.state
+
+finally:
+    fire.stop()
